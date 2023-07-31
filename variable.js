ab4fa6b70c8a1cc4a21b902cd8d340f480fcdc7a
Imran <webimran2021@gmail.com>
2023-07-31 18:32:08 +0600

    added simple task

---
+++ variable.js
@@ -0,0 +1,28 @@
+// practice problem 1
+var momGaveHim = 1000;
+var applesPrice1kg = "x";
+var x = 400;
+var orangesPrice1kg = "y";
+var y = 300;
+
+var totalPrice = (400 + 300);
+console.log(totalPrice);
+
+var shopkeeperReceivedMoney = 1000;
+console.log(shopkeeperReceivedMoney - totalPrice);
+
+// practice problem 2
+var mathematics = 75.25;
+var biology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var bangla = 99.50;
+var totalMarks = (mathematics + biology + chemistry + physics + bangla);
+console.log(totalMarks);
+
+var averageMarks = (totalMarks / 5);
+
+console.log(averageMarks);
+
+
+
