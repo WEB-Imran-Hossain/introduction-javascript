af73b81c96dd338d9e1f4140d952b3a94b9c4262
Imran <webimran2021@gmail.com>
2023-08-01 21:00:21 +0600

    added while loop

---
+++ array-revision.js
@@ -0,0 +1,33 @@
+// declare array
+var tableItems = ["bottol", "pen", "mouse", "sunglass", "paper"]
+// find items 3
+var item3 = tableItems[3]; 
+console.log(item3);
+// replace paper with notebook
+tableItems[4] = "notebook";
+console.log(tableItems);
+// find index of pen
+var penIndex = tableItems.indexOf("pen");
+console.log(penIndex);
+
+// push add table items
+
+// pop remove table items
+
+// 1st position add unshift
+
+// 1st position remove shift
+
+
+// conditionals
+if (tableItems.length > 4) {
+    
+}
+
+else if (tableItems[3] == "pen") {
+    
+}
+
+else{
+
+}
---
+++ while.js
@@ -0,0 +1,26 @@
+// while loop means same work repeat and repeat
+
+// while loop syntax
+while (expression) {
+  //    statement
+}
+
+var roastGiven = 0;
+while (roastGiven < 7) {
+  console.log("Roast Dan, Please !!");
+  // roastGiven = roastGiven + 1;
+  // upline r man r shathe 1 jog korar shorthand
+  // roastGiven += 1;
+
+  // upline r man r shathe 1 kore barbe tar shorthand
+  roastGiven++;
+}
+console.log(roastGiven);
+
+// 2nd time try
+var roastGiven = 0;
+while(roastGiven < 7) {
+  console.log("Roast Dan");
+  console.log(roastGiven);
+  roastGiven++;
+}
