a7f794e155a47c2c8c60e4c82ace7a76e4c55dcd
Imran <webimran2021@gmail.com>
2023-08-01 23:20:45 +0600

    added while loop and for loop

---
+++ for.js
@@ -0,0 +1,46 @@
+// var roastGiven = 0;
+while(roastGiven < 7) {
+  console.log('Roast Dan, Please !!')
+  roastGiven++;
+}
+
+// for loop style apply
+for(var roastGiven = 0; roastGiven < 7 ; roastGiven++){
+    console.log('Roast Dan, Please !!');
+}
+
+// simple version of loop
+// nicher line dia bojhar jonno
+console.log("simple for loop");
+for( var i = 0; i < 7; i++){
+    console.log(i);
+}
+
+
+for(var i = 0; i < 10; i++){
+    console.log(i);
+}
+
+
+for(var i = 1; i < 10; i++){
+    console.log(i);
+}
+
+
+for(var i = 1; i <= 10; i++){
+    console.log(i);
+}
+
+
+// odd numbers
+i = i + 2; = i+=2;
+for(var i = 0; i <= 10; i+=2){
+    console.log(i);
+}
+
+
+// even numbers
+// i = i + 2; = i+=2;
+for(var i = 1; i <= 10; i+=2){
+    console.log(i);
+}
