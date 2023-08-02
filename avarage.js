816114ea05ad1b492c90a65c752ef630adc8fc63
Imran <webimran2021@gmail.com>
2023-08-02 19:55:33 +0600

    added all keywords

---
+++ avarage.js
@@ -0,0 +1,13 @@
+// practice problems 2
+var matheMarks = 75.25;
+var biologyMarks = 65;
+var chemistryMarks = 80;
+var physicsMarks = 35.45;
+var banglaMarks = 99.50;
+
+var totalMarks = matheMarks + biologyMarks + chemistryMarks + physicsMarks +banglaMarks;
+console.log(totalMarks);
+var average = totalMarks / 5;
+var averageTwoDecimal = average.toFixed(2)
+console.log(averageTwoDecimal);
+
